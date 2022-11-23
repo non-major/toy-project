2fbbb8eb2dbd27bc6404434471010a95cffe47d1
sujeong ko <dev.sjko@gmail.com>
2022-11-23 23:06:29 +0900

    [fix] merge conflict

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import Home from "./pages/Home";
 import NewContent from "./pages/NewContent";
 import EditContent from "./pages/EditContent";
@@ -9,22 +8,16 @@ import MyDiaryList from "./pages/MyDiaryList";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<NewContent />} />
+        <Route path="/edit" element={<EditContent />} />
+        <Route path="/mydiary" element={<MyDiaryList />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
