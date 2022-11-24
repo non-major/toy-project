05ef370a04c03ad1d4fe37d67634bee9d75cab9d
sujeong ko <dev.sjko@gmail.com>
2022-11-25 01:52:28 +0900

    [feat] NewContent page 마크업 완료

--- front/src/pages/NewContent.jsx
+++ front/src/pages/NewContent.jsx
@@ -1,18 +1,14 @@
-import React, {useState, useRef} from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
-import Input from '../components/Input.jsx'
+// import Input from '../components/Input.jsx'
 import MyButton from '../components/MyButton.jsx';
-import Header from '../components/MyHeader.jsx'
 
 function NewContent() {
 
-    const titleInput = useRef();
-
     const [state, setState] = useState({
-        title: titleInput.current,
+        title: "",
         content:"",
     })
-    console.log(titleInput);
     const handleChangeState = (e) => {
         setState({...state,
             [e.target.name]: e.target.value}
@@ -26,33 +22,55 @@ function NewContent() {
 
 
     return (
-        <>
-            <Header />
-            <section>
-            <Input ref={titleInput} value={state.title} name="title" label="제목" placeholder="제목" />
+        <div>
+            <p><label htmlFor='title'>제목</label></p>
+            <TitleInput name="title" id="title" placeholder="제목을 적어주세요." value={state.title} onChange={handleChangeState}/>
+            <p><label htmlFor='search'>책 이미지 검색하기</label></p>
+            <div>
+            <SearchInput name="search" id="search" placeholder="어떤 책을 읽으셨나요?" onChange={handleChangeState}/>
+            <MyButton text="검색" type="basic" onClick={handleSubmit}/>
+            </div>
             <p>내용</p>
             <ContentInput name="content" placeholder='내용을 적어주세요.' value={state.content} onChange={handleChangeState} />
-            <div>
+            <ButtonWrap>
             <MyButton text="저장하기" type="submit" onClick={handleSubmit}/>
             <MyButton text="작성취소" type="basic"/>
-            </div>
-            </section>
-  
-
-        </>
+            </ButtonWrap>
+        </div>
     );
 }
 
+
+const TitleInput = styled.input`
+width: 500px;
+height: 30px;
+margin: 10px 0;
+padding: 5px 10px;
+border: 1px solid grey;
+border-radius: 8px;
+&:focus{
+    outline: 2px solid #003c8f;
+}`
+
 const ContentInput = styled.textarea`
-width : 500px;
+width: 500px;
 height: 300px;
-margin: 10px;
+margin: 10px 0;
 padding: 10px;
 border: 1px solid grey;
 border-radius: 8px;
 &:focus{
     outline: 2px solid #003c8f;
 }
+`
+const SearchInput = styled(TitleInput)`
+width: 400px;
+margin-right: 20px;
+`
+
+const ButtonWrap = styled.div`
+display: flex;
+justify-content: flex-end;
 `
 
 export default NewContent;
