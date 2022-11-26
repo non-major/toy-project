83f0f26cbfd938875eb88be26018c445f30aa38b
JiYun <jo522115@gmail.com>
2022-11-26 18:06:31 +0900

    [fix]: header 수정

--- front/src/components/MyHeader.jsx
+++ front/src/components/MyHeader.jsx
@@ -7,10 +7,11 @@ const MyHeader = () => {
   //   return (
   //     <div className="nav">
   //       <ul>
+  //         <Link to="/mycontents">내 독서 기록</Link>
+  //         <Link to="/mypage">마이페이지</Link>
   //         <Link to="/" onClick={() => sessionStorage.removeItem("userToken")}>
   //           로그아웃
   //         </Link>
-  //         <Link to="/mypage">마이페이지</Link>
   //       </ul>
   //     </div>
   //   );
