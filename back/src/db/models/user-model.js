42a37b90e643c69f025b744dbd0788a548e10e88
yisuho <yeek0620@gmail.com>
2022-11-30 02:57:33 +0900

    feat:유저조회,전체조회

--- back/src/db/models/user-model.js
+++ back/src/db/models/user-model.js
@@ -1,41 +1,41 @@
 import { model } from "mongoose";
 import { UserSchema } from "../schemas/user-schema.js";
 
-const User =model("users",UserSchema);
+const User = model("users", UserSchema);
 
-export class UserModel{
+export class UserModel {
+  async create(userInfo) {
+    return await User.create(userInfo);
+  }
 
-   async create(userInfo){
-    return await User.create(userInfo)
-   }
+  async findByEmail(email) {
+    return await User.findOne({ email });
+  }
 
-   async findByEmail(email){
-    return await User.findOne({email})
-   }
-   
-   async findByNickname(nickname){
-      return await User.findOne({nickname})
-     }
+  async findByNickname(nickname) {
+    return await User.findOne({ nickname });
+  }
 
-   async findById(userId){
-      return await User.findOne({_id:userId})
-   }
-   
-   async update(userId,update){
-    const filter = { _id:userId};
-    const option = {returnOriginal:false};
+  async findById(userId) {
+    return await User.findOne({ _id: userId });
+  }
 
-   return await User.findByIdAndUpdate(filter, update, option)
-   }
+  async findAll() {
+    return await User.find({});
+  }
 
-   async delete(userId){
-return await User.findByIdAndDelete({_id:userId})
-   }
+  async update(userId, update) {
+    const filter = { _id: userId };
+    const option = { returnOriginal: false };
 
+    return await User.findByIdAndUpdate(filter, update, option);
+  }
 
-
+  async delete(userId) {
+    return await User.findByIdAndDelete({ _id: userId });
+  }
 }
 
 const userModel = new UserModel();
 
-export {userModel}
+export { userModel };
