ab1f4acc83c9d3b28e4ec0ef2bd00ea73ab91ad7
JiYun <jo522115@gmail.com>
2022-12-01 21:43:48 +0900

    [fix]: 글쓰기 버튼 수정

--- front/src/components/CreateBtn.jsx
+++ front/src/components/CreateBtn.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import pencil from "../asset/pencil.png";
 
 const CreateBtn = () => {
   const navigate = useNavigate();
