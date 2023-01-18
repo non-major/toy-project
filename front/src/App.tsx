36459022239ee213add0e3eb14c606cd634d7859
sujeong ko <dev.sjko@gmail.com>
2023-01-18 21:36:53 +0900

    refactor: redux Provider 삭제

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import { useContext, createContext, useReducer } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home/Home";
@@ -17,43 +16,6 @@ import { worker } from "./mocks/browers";
 
 if (process.env.NODE_ENV === "development") {
   worker.start();
-}
-
-import { modalReducer, ModalDispatch, ModalState } from "./redux/reducer";
-
-import PrivateRoute from "./route/PrivateRoute";
-// state 컨텍스트와 dispatch 컨텍스트 생성
-export const StateContext = createContext<ModalState | null>(null);
-export const DispatchContext = createContext<ModalDispatch | null>(null);
-
-// 모든 페이지에 컨텍스트 전달
-export function Provider({ children }: { children: React.ReactNode }) {
-  const [state, dispatch] = useReducer(modalReducer, {
-    isReportModalOpen: false,
-  });
-  return (
-    <StateContext.Provider value={state}>
-      <DispatchContext.Provider value={dispatch}>
-        {children}
-      </DispatchContext.Provider>
-    </StateContext.Provider>
-  );
-}
-
-export function useModalState() {
-  const state = useContext(StateContext);
-  if (!state) {
-    throw new Error("Cannot find Provider");
-  } // 유효하지 않을땐 에러를 발생
-  return state;
-}
-
-export function useModalDispatch() {
-  const dispatch = useContext(DispatchContext);
-  if (!dispatch) {
-    throw new Error("Cannot find Provider");
-  } // 유효하지 않을땐 에러를 발생
-  return dispatch;
 }
 
 const queryClient = new QueryClient();
