f61c737eb5c2e05886d02fd59e7003c619c7546a
sujeong ko <dev.sjko@gmail.com>
2023-02-01 02:20:28 +0900

    fix: 로그인 안했을 때 댓글 에러 처리

--- front/src/api/postOneComment.tsx
+++ front/src/api/postOneComment.tsx
@@ -1,5 +1,6 @@
 import { instance } from "./axiosInstance";
 import axios from "axios";
+import { redirect } from "react-router-dom";
 
 type CommentData = {
   content: string;
@@ -14,10 +15,17 @@ type PostCommentFn = {
 };
 
 const postOneComment: PostCommentFn = async (id, body) => {
+  if (!sessionStorage.getItem("userToken")) {
+    alert("로그인 한 사용자만 댓글을 작성할 수 있습니다.");
+    window.scrollTo(0, 0);
+    return;
+  }
+
   if (body.content === "") {
     alert("댓글 내용을 입력해주세요.");
     return;
   }
+
   try {
     const { data } = await instance.post(`/api/comments/${id}`, {
       content: body.content,
@@ -25,9 +33,6 @@ const postOneComment: PostCommentFn = async (id, body) => {
     });
     return data;
   } catch (err) {
-    if (axios.isAxiosError(err) && err.response) {
-      alert(`로그인 한 사용자만 사용할 수 있는 기능입니다.`);
-    }
     return err;
   }
 };
