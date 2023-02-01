9d1611c7efcc4fcb3484ac1b600aa57f6efa26ae
sujeong ko <dev.sjko@gmail.com>
2023-02-01 23:44:28 +0900

    fix: 작성자 여부 확인 수정

--- front/src/api/getOnePost.tsx
+++ front/src/api/getOnePost.tsx
@@ -1,8 +1,12 @@
 import { instance } from "./axiosInstance";
 
 const getOnePost = async (id: string | undefined) => {
-  const postData = await instance.get(`/api/posts/${id}`);
-  return postData;
+  try {
+    const postData = await instance.get(`/api/posts/${id}`);
+    return postData;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default getOnePost;
